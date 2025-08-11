a1e61e50e8629dd6c67aa09c38e1d7e7b8250ca4
Sangmin Han <gkstkdals07@naver.com>
2025-08-11 15:17:56 +0900

    :coffin: Remove: 필요없는 파일 삭제

--- src/main/java/com/kbai/tradejoe/repository/AnalysisRepository.java
+++
@@ -1,7 +0,0 @@
-package com.kbai.tradejoe.repository;
-
-import com.kbai.tradejoe.domain.Analysis;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AnalysisRepository extends JpaRepository<Analysis, Long> {
-}
